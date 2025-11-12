26a57795340d08d81d270a2b533093402be80e9c
андрей <andrej.korolev.04@mail.ru>
2025-11-12 16:03:36 +0300

    Рефакторинг старого проекта приложения криптовалют

---
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.cryptoapp.presentation
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.cryptoapp.data.network.ApiFactory
+import com.example.cryptoapp.data.database.AppDatabase
+import com.example.cryptoapp.data.network.model.CoinInfoDto
+import com.example.cryptoapp.data.network.model.CoinInfoJsonContainerDto
+import com.example.cryptoapp.data.repository.CoinRepositoryImpl
+import com.example.cryptoapp.domain.GetCoinInfoListUseCase
+import com.example.cryptoapp.domain.GetCoinInfoUseCase
+import com.example.cryptoapp.domain.LoadDataUseCase
+import com.google.gson.Gson
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+
+class CoinViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository = CoinRepositoryImpl(application)
+
+    private val getCoinInfoListUseCase = GetCoinInfoListUseCase(repository)
+    private val getCoinInfoUseCase = GetCoinInfoUseCase(repository)
+    private val loadDataUseCase = LoadDataUseCase(repository)
+
+    val coinInfoList = getCoinInfoListUseCase()
+
+    fun getDetailInfo(fSym: String) = getCoinInfoUseCase(fSym)
+
+    init {
+        loadDataUseCase()
+    }
+}
